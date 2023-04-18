64b4a2f8cb1fa943dab244e173133e50212f3497
Akash Tiwari <124699923+akash07t@users.noreply.github.com>
2023-04-18 14:07:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+    
 function volume_sphere() {
     //Write your code here
+	let r = document.getElementById('radius');
+	let v = document.getElementById('volume');
+	
+	let res = (r.value*r.value*r.value*Math.PI*4)/3;
+	v.value = res;
+	
+	
   
 } 
 
